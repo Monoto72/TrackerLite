ceabce8f9b54fa3bcb39fcad0096030f5ce70b6f
UP2017778 <sking233952@gmail.com>
2026-09-07 18:59:56 +0100

    feat: MySQL2 implementation with pooling check

--- server/eslint.config.ts
+++ server/eslint.config.ts
@@ -1,8 +1,15 @@
 import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default defineConfig({
   files: ['**/*.{js,ts}'],
   extends: [js.configs.recommended, tseslint.configs.recommended],
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+  },
+  ignores: ['dist/**'],
 });
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import express, { type Express, type Request, type Response } from 'express';
 import log4js from 'log4js';
+import './utils/loadEnv.js'; 
+import {testConnection} from './config/db.js'
 
 const { getLogger, configure } = log4js;
 
@@ -21,17 +23,20 @@ configure({
     },
 });
 
-const PORT: number = 3000;
-
 const app: Express = express();
 const logger = getLogger();
+
+testConnection().then(() => {
+    app.listen(process.env.PORT, () => {
+        logger.info(`Server running on http://127.0.0.1:${process.env.PORT}`);
+    });
+}).catch((err: Error) => {
+    logger.error(`Failled to connect to database! Exiting: ${err}`);
+    process.exit(1);
+})
 
 
 app.get('/', (req: Request, res: Response) => {
 
     res.send('test')
 })
-
-app.listen(PORT, () => {
-    logger.info(`Server running on http://127.0.0.1:${PORT}`);
-});
